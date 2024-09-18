38850e9ebd4cac2d8149aeb6ab21565687b70177
Bishop Shaw III <prototype124@yahoo.com>
2024-09-17 19:59:50 -0700

    added new (SQL insert) method for companies route

--- timeclockDjangoApp/views/companies_views.py
+++ timeclockDjangoApp/views/companies_views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from django.db import connection
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 import json
 
 
 
 # shift routes
-@login_required
+# @login_required
 def company_index(request):
 
     # get user session
@@ -16,7 +17,7 @@ def company_index(request):
 
     if request.method == "GET":
 
-        if loggedIn_user_id is not None:
+        if not loggedIn_user_id:
             with connection.cursor as cursor:
                 cursor.execute('SELECT * FROM shifts WHERE user_id = %s', [loggedIn_user_id])
                 shifts = cursor.fetchall()
@@ -31,7 +32,43 @@ def company_index(request):
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
 
 def new_company(request, id):
-    return HttpResponse("shifts")
+    # return HttpResponse("shifts")
+    if request.method == 'POST':
+        user = request.session.get('user_id')
+
+        data = json.loads(request.body)
+
+        name = data.POST.get('name')
+        start_time = data.POST.get('start_time', '').strip()
+        end_time = data.POST.get('end_time', '').strip()
+        pay_rate = data.POST.get('pay_rate', '').strip()
+        start_date = data.POST.get('start_date', '').strip()
+
+        current_time = datetime.datetime.now()
+        formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S') 
+
+        if not user:
+            return JsonResponse({'status': 'error', 'message': 'User needs to login!'})
+        
+        try:
+            if name and start_time and end_time and pay_rate and start_date:
+                with connection.cursor() as cursor:
+                    cursor.execute('''
+                                INSERT INTO companies 
+                                (user_id, name, start_time, end_time, pay_rate, start_date, created_on) 
+                                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                                ''', [user, name, start_time, end_time, pay_rate, start_date, formatted_time])
+                    if cursor.rowcount > 0:
+                        return JsonResponse({'status': 'success', 'message': 'New Company Created Successfully!'})
+                    else:
+                        return JsonResponse({'status': 'error', 'message': 'Something Went Wrong. Company Was Not Created Successfully!'})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Field(s) is required'})
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON data'}, status=400)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message':str(e)})
+    return JsonResponse({'status': 'error', 'message': 'Request Method Invalid'})
 
 def company_details(request, id):
    # return HttpResponse("shifts")
